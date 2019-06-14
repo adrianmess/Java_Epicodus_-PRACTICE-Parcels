3ac3b4b5edccd8191653195eed63a9130ef12296
Adrian Messado <adrian.messado@gmail.com>
2019-06-14 11:46:46 -0700

    add length, width, height attributes to Parcel class and  create constructor for Parcels class

--- src/main/java/models/Parcels.java
+++ src/main/java/models/Parcels.java
@@ -1,4 +1,13 @@
 package models;
 
 public class Parcels {
+    public int length;
+    public int width;
+    public int height;
+
+    public Parcels(int length, int width, int height) {
+        this.length = length;
+        this.width = width;
+        this.height = height;
+    }
 }
