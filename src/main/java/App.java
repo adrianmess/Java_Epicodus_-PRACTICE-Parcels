e6d8d2a620bac839d84f006801f1554ac021c0c8
Adrian Messado <adrian.messado@gmail.com>
2019-06-14 15:00:52 -0700

    add boolean programRunning set to true, place everything with try/catch in while statement. replace string comparison operator with .equals method

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -16,15 +16,15 @@ public class App {
         int wrapCost = 0;
         double weightCost = weight * .25;
 
-        if (speed == "priority") {
+        if (speed.equals("priority")) {
             costToShip += 9;
-        } else if (speed == "ground") {
+        } else if (speed.equals("ground")) {
             costToShip += 3;
         }
 
-        if (giftWrap == "yes") {
+        if (giftWrap.equals("yes")) {
             costToShip +=4;
-        } else if (giftWrap =="no" ) {
+        } else if (giftWrap.equals("no")) {
             costToShip +=0;
         }
         double finalCost = costToShip + volumeCost + wrapCost + weightCost;
@@ -35,44 +35,55 @@ public class App {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader (System.in));
         System.out.println("Please provide dimensions of package. Length, Width, Height & Weight:");
 
+        boolean programRunning = true;
+
+        while (programRunning) {
+            try {
+                System.out.println("Length: ");
+                int length = Integer.parseInt(bufferedReader.readLine());
+
+                System.out.println("Width: ");
+                int width = Integer.parseInt(bufferedReader.readLine());
+                System.out.println("Height: ");
+
+                int height = Integer.parseInt(bufferedReader.readLine());
+                System.out.println("Weight (in lbs): ");
+
+                double lbs = Integer.parseInt(bufferedReader.readLine());
+                int volume = calcVolume(length, width, height);
+
+                System.out.println("The volume of your package is: " + volume);
+
+                System.out.println(" ");
+                System.out.println("Please type the shipping speed you want:");
+                System.out.println("'Priority' (1-3 days):");
+                System.out.println("'Ground' (3-7 days):");
+                String speed = bufferedReader.readLine().toLowerCase();
+
+                System.out.println(" ");
+                System.out.println("Would you like gift wrapping?  Yes or No?");
+                String giftWrap = bufferedReader.readLine().toLowerCase();
+
+                System.out.println(" ");
+                System.out.println("Your total shipping price comes to ");
 
 
-        try {
-            System.out.println("Length: ");
-            int length = Integer.parseInt(bufferedReader.readLine());
-            
-            System.out.println("Width: ");
-            int width = Integer.parseInt(bufferedReader.readLine());
-            System.out.println("Height: ");
-            
-            int height = Integer.parseInt(bufferedReader.readLine());
-            System.out.println("Weight (in lbs): ");
-            
-            double lbs = Integer.parseInt(bufferedReader.readLine());
-            int volume = calcVolume(length, width, height);
-            
-            System.out.println("The volume of your package is: " + volume);
+                double finalCost = costToShip(volume, speed, giftWrap, lbs);
+                System.out.println(finalCost);
 
-            System.out.println(" ");
-            System.out.println("Please type the shipping speed you want:");
-            System.out.println("'Priority' (1-3 days):");
-            System.out.println("'Ground' (3-7 days):");
-            String speed = bufferedReader.readLine().toLowerCase();
-
-            System.out.println(" ");
-            System.out.println("Would you like gift wrapping?  Yes or No?");
-            String giftWrap = bufferedReader.readLine().toLowerCase();
-
-            System.out.println(" ");
-            System.out.println("Your total shipping price comes to ");
-
-//            System.out.println(" ");
-//            System.out.println("Would you like to ship another item? ");
-            double finalCost = costToShip(volume, speed, giftWrap, lbs);
-            System.out.println(finalCost);
-
-        } catch(IOException e) {
-            e.printStackTrace();
+                System.out.println("Type 'exit' to quite or hit return to start new order");
+                String newOrderOrExit = bufferedReader.readLine();
+                System.out.println(newOrderOrExit);
+                if (newOrderOrExit.equals("exit")) {
+                     programRunning = false;
+                }
+            } catch(IOException e) {
+                e.printStackTrace();
+            }
         }
-    }
+
+
+
+        }
+
 }
