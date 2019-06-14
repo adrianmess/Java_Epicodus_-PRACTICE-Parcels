0bc7d74af74e60f7ceec088c150400b550053601
Adrian Messado <adrian.messado@gmail.com>
2019-06-14 11:49:12 -0700

    add weight attribute to Parcel class and constructor

--- src/main/java/models/Parcels.java
+++ src/main/java/models/Parcels.java
@@ -4,10 +4,12 @@ public class Parcels {
     public int length;
     public int width;
     public int height;
+    public int weight;
 
-    public Parcels(int length, int width, int height) {
+    public Parcels(int length, int width, int height, int weight) {
         this.length = length;
         this.width = width;
         this.height = height;
+        this.weight = weight;
     }
 }
