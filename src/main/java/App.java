baa3226ba8fdec92361811917b651e566afef52f
Adrian Messado <adrian.messado@gmail.com>
2019-06-14 12:02:56 -0700

    prompt user for length, width, height, weight. then store responses in appropriate variables

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,6 +8,14 @@ public class App {
         System.out.println("Please provide dimensions of package. Length, Width, Height & Weight:");
 
         try {
+            System.out.println("Length: ");
+            int length = Integer.parseInt(bufferedReader.readLine());
+            System.out.println("Width: ");
+            int width = Integer.parseInt(bufferedReader.readLine());
+            System.out.println("Height: ");
+            int height = Integer.parseInt(bufferedReader.readLine());
+            System.out.println("Weight (in lbs): ");
+            double lbs = Integer.parseInt(bufferedReader.readLine());
 
         } catch(IOException e) {
             e.printStackTrace();
