3068e12c6e7edc1c7f75b14fd014617002fc0c78
Adrian Messado <adrian.messado@gmail.com>
2019-06-14 11:58:12 -0700

    create try/catch block with IOexception, import IOException library

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,16 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class App {
     public static void main(String[] args){
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader (System.in));
         System.out.println("Please provide dimensions of package. Length, Width, Height & Weight:");
+
+        try {
+
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
     }
 }
