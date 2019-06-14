4e07c66a12b048ce86bdaf8d24f173299dc35e6c
Adrian Messado <adrian.messado@gmail.com>
2019-06-14 11:55:39 -0700

    prompt user for package dimensions

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,6 +4,6 @@ import java.io.InputStreamReader;
 public class App {
     public static void main(String[] args){
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader (System.in));
-
+        System.out.println("Please provide dimensions of package. Length, Width, Height & Weight:");
     }
 }
